6e042c09b86cac3d081fc3a67714a54267cd5dbf
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2020-05-02 17:20:35 -0400

    Handle custom routes.

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  const { url } = req;
+  const json = { url };
+
   res.setHeader("Content-Type", "application/json");
 
-  const json = {
-    url: req.url,
-  };
+  if (url === "/") {
+    json.message = "Welcome to the root";
+  } else {
+    json.message = `You accessed a specified route: ${url}`;
+  }
 
   res.write(JSON.stringify(json));
   res.end();
