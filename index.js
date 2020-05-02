bf98504d7dc0a5c30b9af00e9ca03574389b830e
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2020-05-02 17:13:07 -0400

    Send url path of request being invoked on server.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url);
+  res.setHeader("Content-Type", "application/json");
+
+  const json = {
+    url: req.url,
+  };
+
+  res.write(JSON.stringify(json));
+  res.end();
 });
 
 server.listen(3000);
